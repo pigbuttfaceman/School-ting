7c95863b7423c7b7f1c08a96b00483ea4604fe9b
nugg man <37384979+pigbuttfaceman@users.noreply.github.com>
2019-02-15 12:33:22 +1300

    Add files via upload

---
+++ first.py
@@ -0,0 +1,11 @@
+
+
+
+
+
+tickets = [
+    Ticket("Ethan Stace", "staceethan@gmail.com", "26/07/02", False),
+    Ticket("Bob Ross", "bobross@gmail.com", "27/07/02", False),
+    Ticket("Steve Ewrin", "STEVE@gmail.com", "25/01/89", False),
+    Ticket("Morgan Freeman", "sooth@gmail.com", "18/02/69", False)
+    ]
