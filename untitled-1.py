65957c7bff1c5d57f96bbe9770c3a33bafbff6b5
nugg man <37384979+pigbuttfaceman@users.noreply.github.com>
2019-03-01 12:41:41 +1300

    Add files via upload

--- untitled-1.py
+++ untitled-1.py
@@ -1,5 +1,9 @@
 # School-ting
-
+#ver 1.0
+#ver 1.2
+#ver 1.3
+#ver 1.4
+#ver 1.5
 
 from bottle import run, route, view, get, post, request
 from itertools import count
@@ -11,15 +15,15 @@ class Ticket:
     def __init__(self, name, email, date_of_birth, check_in):
         self.id = next(self._ids)
         self.name = name
-        self_email = email
-        self_dob = date_of_birth
-        self_check_in = check_in
+        self.email = email
+        self.dob = date_of_birth
+        self.check_in = check_in
         
 tickets = [
     Ticket("Ethan Stace", "staceethan@gmail.com", "26/07/02", False),
-    Ticket("Bob Ross", "bobross@gmail.com", "27/07/02", False),
+    Ticket("Bob Ross", "bobross@gmail.com", "27/07/02", True),
     Ticket("Steve Ewrin", "STEVE@gmail.com", "25/01/89", False),
-    Ticket("Morgan Freeman", "sooth@gmail.com", "18/02/69", False)
+    Ticket("Bill Cosby", "freedrinks@gmail.com", "18/02/69", True)
     ]
 
 
@@ -31,6 +35,40 @@ def index():
     pass
 
 
+#check in
+@route("/check-in")
+@view ("check-in")
+def check_in():
+    data = dict (ticket_list = tickets)
+    return data
+
+@route('/check-in-success/<ticket_id>')
+@view ('check-in-success')
+def check_in_success(ticket_id):
+    
+    ticket_id = int(ticket_id)
+    found_ticket = None
+    for ticket in tickets:
+        if ticket.id == ticket_id:
+            found_ticket = ticket
+    data = dict (ticket = found_ticket)
+    found_ticket.check_in = True
+    return data
+
+@route("/sign-up")
+@view ("sign-up")
+def sign_up():
+    pass
+    
+@route("/sign-up-success")
+@view ("sign-up-success")
+def sign_up_success():
+    name = request.forms.get('name')
+    email = request.forms.get('email')
+    date_of_birth = request.forms.get('dob')
+    
+    new_ticket = Ticket(name, email, date_of_birth, False)
+    tickets.append(new_tickets)
 
 
 
